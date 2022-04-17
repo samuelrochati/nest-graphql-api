25cb67221766521ac49bf590bda5eb7526c2eb96
Samuel Rocha <samuelrocha.ti@hotmail.com>
2022-04-17 17:14:18 -0300

    feat: implement service of user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserInput } from './dto/create-user.input';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async createUser(data: CreateUserInput): Promise<User> {
+    const user = await this.userRepository.create(data);
+    const userSave = await this.userRepository.save(user);
+
+    if (!userSave) {
+      throw new InternalServerErrorException('Problema para criar um usuário.');
+    }
+
+    return userSave;
+  }
+}
