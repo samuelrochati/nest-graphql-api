eee0c39032af8de4d58d9ae55708f4270aba3fdc
Samuel Rocha <samuelrocha.ti@hotmail.com>
2022-04-18 21:14:54 -0300

    chore: folder entities

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,7 +1,7 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
-import { User } from './user.entity';
+import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
 @Resolver()
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,7 +7,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
-import { User } from './user.entity';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
